934207fcf9af1236e9b96168981af85ba35f8692
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-22 22:09:23 +0000

    write and pass test for rating buttons
    homepage now has basic html buttons wrapped in css
    form exists for storing data

--- spec/features/rating_home_spec.rb
+++ spec/features/rating_home_spec.rb
@@ -1,7 +1,12 @@
 feature 'Ratings Home Page' do
-  scenario 'Should visit the homepage and see a welcome message' do
+  scenario 'Should visit the homepage and see an invitation' do
     visit '/'
     expect(page.status_code).to eq 200
     expect(page).to have_content('Please share your experience with us')
+  end
+
+  scenario 'the homepage has buttons to leave a rating' do
+    visit '/'
+    expect(page).to have_css('button', count: 5)
   end
 end
