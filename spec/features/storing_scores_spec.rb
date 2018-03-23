9f6f1b2edf950f27e3b1a49781bc597efa462b72
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-23 22:10:35 +0000

    alter html on thanks view to show stats
    a percentage for each rating is now shown
    logic can potentially be refactored later

--- spec/features/storing_scores_spec.rb
+++ spec/features/storing_scores_spec.rb
@@ -1,16 +1,13 @@
 feature 'viewing scores' do
-  scenario 'A user can see scores' do
-    visit('/')
-    click_button(-2)
-    click_button('Return')
-    click_button(0)
-    expect(page).to have_content "-2"
-    expect(page).to have_content "0"
-  end
-
   scenario 'A users score is stored in the database' do
     visit('/')
     click_button(-1)
     expect(page).to have_content "-1"
+  end
+
+  scenario 'A user can see stats on previous entries on the thank you page' do
+    visit('/')
+    store_many_ratings
+    expect(page).to have_content "%"
   end
 end
