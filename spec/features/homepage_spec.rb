263693310d3a661182d9130b38546fe9044c3088
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-23 10:39:25 +0000

    write test for leaving a score
    test checks that on clicking a button user is redirected
    button type changed to submit in html

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -9,4 +9,10 @@ feature 'Ratings Home Page' do
     visit '/'
     expect(page).to have_css('button', count: 5)
   end
+
+  scenario 'leaving a score' do
+    visit '/'
+    click_button(0)
+    expect(page).to have_content('Thanks for sharing your experience with us')
+  end
 end
