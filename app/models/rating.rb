bd31f60ead1fab0fb36eee209ed11916ea131ee3
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-23 18:13:44 +0000

    set up test environment and database clean before running tests

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,7 +3,12 @@ require 'pg'
 class Rating
 
   def self.all
-    connection = PG.connect(dbname: 'ft_ratings')
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'ft_ratings_test')
+    else
+      connection = PG.connect(dbname: 'ft_ratings')
+    end
+    
     result = connection.exec("SELECT * FROM ratings")
     result.map { |rating| rating['score'].to_i }
   end
