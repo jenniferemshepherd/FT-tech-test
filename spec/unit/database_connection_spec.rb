e790d3e6e2dd38385120ece48d68ef8f0d946d2c
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-25 18:28:40 +0100

    alter tests to reflect new connection to test database

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -3,19 +3,19 @@ require './app/models/database_connection'
 describe DatabaseConnection do
   describe '.setup' do
     it 'sets up a connection to the database through PG' do
-      expect(PG).to receive(:connect).with(dbname: 'ft_ratings_test')
-      DatabaseConnection.setup('ft_ratings_test')
+      expect(PG).to receive(:connect).with('postgres://localhost/ft_ratings_test')
+      DatabaseConnection.setup('postgres://localhost/ft_ratings_test')
     end
 
     it 'has a persistent connection' do
-      connection = DatabaseConnection.setup('ft_ratings_test')
+      connection = DatabaseConnection.setup('postgres://localhost/ft_ratings_test')
       expect(DatabaseConnection.connection).to eq connection
     end
   end
 
   describe '.query' do
     it 'executes a query via PG' do
-      connection = DatabaseConnection.setup('ft_ratings_test')
+      connection = DatabaseConnection.setup('postgres://localhost/ft_ratings_test')
       expect(connection).to receive(:exec).with("SELECT * FROM ratings;")
       DatabaseConnection.query("SELECT * FROM ratings;")
     end
