9baa7bc81f4ccd245a5f234a46e356b771cc8785
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-25 13:12:32 +0100

    change app path AGAIN in procfile

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -1,6 +1,8 @@
 require './app/models/database_connection'
 
+p ENV['DATABASE_URL']
 if ENV['DATABASE_URL']
+  p ENV['DATABASE_URL']
   DatabaseConnection.setup(ENV['DATABASE_URL'])
 elsif ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup('ft_ratings_test')
