006e4ba0818735d1d54ec23a4722c59470abe752
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-03-25 19:25:24 +0100

    refactor webhelper

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,35 +1,13 @@
 def store_many_ratings
-  click_button(-1)
-  click_button('Return')
-  click_button(2)
-  click_button('Return')
-  click_button(2)
-  click_button('Return')
+  9.times { rate(2) }
+  2.times { rate(-1) }
+  2.times { rate(0) }
+  1.times { rate(-2) }
+  7.times { rate(1) }
   click_button(1)
+end
+
+def rate(score)
+  click_button(score)
   click_button('Return')
-  click_button(0)
-  click_button('Return')
-  click_button(1)
-  click_button('Return')
-  click_button(-2)
-  click_button('Return')
-  click_button(2)
-  click_button('Return')
-  click_button(1)
-  click_button('Return')
-  click_button(2)
-  click_button('Return')
-  click_button(2)
-  click_button('Return')
-  click_button(2)
-  click_button('Return')
-  click_button(0)
-  click_button('Return')
-  click_button(-1)
-  click_button('Return')
-  click_button(0)
-  click_button('Return')
-  click_button(1)
-  click_button('Return')
-  click_button(1)
 end
